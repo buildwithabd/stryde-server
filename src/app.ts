cbdab324f02fb294a59c2701c355cc9adc678ebf
AfricanBudu <abdullahagboola202@gmail.com>
2026-02-23 16:03:34 +0100

    Add security features to app.ts by integrating helmet and compression middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
+
+// Security
+app.use(helmet());
+app.use(compression());
 
 app.use(cors());
 app.use(express.json());
